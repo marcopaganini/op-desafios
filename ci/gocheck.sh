ca308ba7bc7e01b75a6e3c3bcbfbfd7149a05991
Marco Paganini <paganini@paganini.net>
2022-12-16 17:31:53 -0800

    Replace golint with golangci-lint.

--- ci/gocheck.sh
+++ ci/gocheck.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 # Test code formatting compliance (Go)
 
-# Run golint on all files in the current directory.
-function go_lint() {
-  golint -set_exit_status 2>&1
-}
-
-# Run govet on all files in the current directory.
-function go_vet() {
-  go vet 2>&1
+# Run golangci_lint on all files in the current directory.
+function golangci__lint() {
+  golangci-lint run -set_exit_status 2>&1
 }
 
 # Run go_fmt in all go files in current dir. Report diffs.
 function go_fmt() {
   # Create a temp file just for this check.
-  local tmp=`mktemp`
+  local tmp
+  tmp="$(mktemp)"
+  # shellcheck disable=SC2064
   trap "rm -f $tmp" EXIT
 
   find . -type f -name '*.go' -exec gofmt -s -l "{}" \; >"$tmp"
@@ -45,12 +42,12 @@ set -o nounset
 # List of all distinct directories having at least one modified with a ".go" extension
 go_dirs=$(grep '\.go$' "$HOME/changed_files.txt" | xargs -l dirname 2>/dev/null | sort -u)
 
-tmpfile=`mktemp`
+tmpfile="$(mktemp)"
+# shellcheck disable=SC2064
 trap "rm -f $tmpfile" EXIT
 
-
 # Save stdout/err in FD 6/7, redirect stdout and stderr to $tmpfile.
-exec 6>&1 >$tmpfile
+exec 6>&1 >"$tmpfile"
 exec 7>&2 2>&1
 
 err=0
@@ -66,19 +63,19 @@ for dir in $go_dirs; do
   export TESTDIR="${GOPATH}/src/test"
   clean_gopath "${GOPATH}"
 
-  pushd "${dir}" >/dev/null
+  pushd "${dir}" || exit 1
   cp -ar . "${TESTDIR}"
-  popd >/dev/null
+  popd || exit 1
 
   # Fetch dependencies.
-  pushd "${TESTDIR}" >/dev/null
+  pushd "${TESTDIR}" || exit 1
   go get -t ./...
 
-  for test in go_lint go_vet go_fmt; do
+  for test in golangci_lint go_fmt; do
     echo -e "\n*** Running test $test"
     $test && echo "No problems found" || err=1
   done
-  popd >/dev/null
+  popd || exit 1
 done
 
 # Restore FDs and close FDs 6/7.
@@ -86,7 +83,7 @@ exec 1>&6 6>&-
 exec 2>&7 7>&-
 
 # Display results of all tests.
-cat $tmpfile
+cat "$tmpfile"
 
 if (( err == 1 )); then
   exit 1
